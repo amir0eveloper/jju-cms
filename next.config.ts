3eca68d030e9f92d93d746143a04899fa6a5cef7
amir0eveloper <amir0eveloper@gmail.com>
2026-01-18 18:32:59 +0300

    Fixed Vercel Errors Phase 2

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname),
-      "@/": path.resolve(__dirname),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
